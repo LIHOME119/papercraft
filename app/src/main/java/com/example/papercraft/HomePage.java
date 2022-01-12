f0e34c183f8610aaab935f5f3c8e41b8edbee8fe
lihome <takfu27@gmail.com>
2022-01-13 00:48:59 +0800

    google map api and google map activtiy testing

--- app/src/main/java/com/example/papercraft/HomePage.java
+++ app/src/main/java/com/example/papercraft/HomePage.java
@@ -15,7 +15,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class HomePage extends AppCompatActivity {
 
     TextView homeLoginName_tv, homeLoginMail_tv;
-    Button logout_btn;
+    Button logout_btn, map_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,14 @@ public class HomePage extends AppCompatActivity {
         homeLoginMail_tv=findViewById(R.id.tv_homeLoginMail);
         homeLoginName_tv=findViewById(R.id.tv_homeLoginName);
         logout_btn=findViewById(R.id.btn_logout);
+        map_btn=findViewById(R.id.btn_map);
+        map_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount != null){
             homeLoginName_tv.setText(signInAccount.getDisplayName());
