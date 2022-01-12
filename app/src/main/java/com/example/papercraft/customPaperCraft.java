bd262234b72469b7cb055c2e87005f9077b09f30
lihome <takfu27@gmail.com>
2022-01-12 16:58:58 +0800

    Imagelabeling and basic ui

--- app/src/main/java/com/example/papercraft/customPaperCraft.java
+++ app/src/main/java/com/example/papercraft/customPaperCraft.java
@@ -1,21 +1,39 @@
 package com.example.papercraft;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.IOException;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class customPaperCraft extends AppCompatActivity {
 
     ImageView CustomPCimageView;
     Button SelectCustomPC_btn;
+    TextView CustomPCimagelabelingResult;
+    InputImage inputImage;
+    ImageLabeler labeler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +41,8 @@ public class customPaperCraft extends AppCompatActivity {
 
         CustomPCimageView = findViewById(R.id.CustomPCimage);
         SelectCustomPC_btn = findViewById(R.id.btn_SelectCustomPCimage);
+        CustomPCimagelabelingResult = findViewById(R.id.tv_CustomPCimageLabelingResult);
+        labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
         SelectCustomPC_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,7 +62,12 @@ public class customPaperCraft extends AppCompatActivity {
                 if (result != null) {
                     Uri uri = result.getUri();
                     CustomPCimageView.setImageURI(uri);
-
+                    try {
+                        inputImage = InputImage.fromFilePath(customPaperCraft.this, uri);
+                        processImage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                 }
 
@@ -50,5 +75,25 @@ public class customPaperCraft extends AppCompatActivity {
         }
 
 
+    }
+
+    private void processImage() {
+        labeler.process(inputImage).addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+            @Override
+            public void onSuccess(@NonNull List<ImageLabel> imageLabels) {
+                String result = "";
+                for (ImageLabel label : imageLabels){
+                    result=result+"\n"+label.getText();
+                }
+                CustomPCimagelabelingResult.setText(result);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: "+e.getMessage());
+
+            }
+        });
     }
 }
