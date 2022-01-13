11dc85c3d3aa2bc905e2f3efb04061ff5661473a
lihome <takfu27@gmail.com>
2022-01-13 21:00:43 +0800

    login page

--- app/src/main/java/com/example/papercraft/LoginPage.java
+++ app/src/main/java/com/example/papercraft/LoginPage.java
@@ -2,12 +2,15 @@ package com.example.papercraft;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -26,6 +30,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 import static android.content.ContentValues.TAG;
 
 public class LoginPage extends AppCompatActivity implements SignUpFragment.BottomSheetListener {
@@ -34,7 +43,8 @@ public class LoginPage extends AppCompatActivity implements SignUpFragment.Botto
     private static final int RC_SIGN_IN = 123;
     private GoogleSignInClient mGoogleSignInClient;
 
-    Button login_btn, signup_btn;
+    SignInButton login_btn;
+    Button  signup_btn;
     private FirebaseAuth mAuth;
 
     @Override
@@ -50,6 +60,12 @@ public class LoginPage extends AppCompatActivity implements SignUpFragment.Botto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Window window = this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, android.R.color.transparent));
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         mAuth = FirebaseAuth.getInstance();
@@ -71,6 +87,14 @@ public class LoginPage extends AppCompatActivity implements SignUpFragment.Botto
                 signIn();
             }
         });
+
+        GifImageView imageView1=findViewById(R.id.login_background);
+        try {
+            GifDrawable gifDrawable = new GifDrawable(getResources(), R.drawable.burning2);
+            imageView1.setImageDrawable(gifDrawable);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -129,6 +153,7 @@ public class LoginPage extends AppCompatActivity implements SignUpFragment.Botto
     @Override
     public void onButtonClicked(String text) {
 
-        login_btn.setText("haha");
+        Toast.makeText(LoginPage.this,text,Toast.LENGTH_SHORT).show();
+
     }
 }
