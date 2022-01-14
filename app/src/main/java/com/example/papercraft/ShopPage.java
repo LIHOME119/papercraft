610f18bb921ff54c36380a3ed71a22e8b5027a47
lihome <takfu27@gmail.com>
2022-01-14 14:36:23 +0800

    shop counting

--- app/src/main/java/com/example/papercraft/ShopPage.java
+++ app/src/main/java/com/example/papercraft/ShopPage.java
@@ -1,7 +1,9 @@
 package com.example.papercraft;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,8 +15,8 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 public class ShopPage extends AppCompatActivity {
 
     ImageButton god1_ibtn, god1_ibtnA, god2_ibtn, god2_ibtnA,god3_ibtn, god3_ibtnA;
-    TextView god1_edt,god2_edt,god3_edt;
-    public static int defaltnum1, defaltnum2, defaltnum3;
+    TextView god1_edt,god2_edt,god3_edt, total_tv;
+    public static int defaltnum1, defaltnum2, defaltnum3, totalnum;
     ExtendedFloatingActionButton shopFire_fbtn, shopDone_fbtn;
 
     @Override
@@ -31,8 +33,56 @@ public class ShopPage extends AppCompatActivity {
         god1_edt=findViewById(R.id.edt_god1);
         god2_edt=findViewById(R.id.edt_god2);
         god3_edt=findViewById(R.id.edt_god3);
+        total_tv=findViewById(R.id.tv_shopTotal);
         shopDone_fbtn=findViewById(R.id.fbtn_shopDone);
         shopFire_fbtn=findViewById(R.id.fbtn_shopfire);
+        shopFire_fbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(ShopPage.this);
+                builder.setMessage("Please confirm the shop cart:  \n\n(Assistant customer to do sacrificing)\n"+
+                        "Paper Craft Package 1 x"+defaltnum1+"\n"+
+                        "Paper Craft Package 2 x"+defaltnum2+"\n"+
+                        "Paper Craft Package 3 x"+defaltnum3+"\n"+
+                        "\n Total $"+ totalnum)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                                finish();
+                            }
+                        }).setNegativeButton("No", null);
+
+                AlertDialog aleartDialog=builder.create();
+                aleartDialog.show();
+            }
+        });
+        shopDone_fbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UpdateTotal();
+
+                AlertDialog.Builder builder=new AlertDialog.Builder(ShopPage.this);
+                builder.setMessage("Please confirm the shop cart:  \n\n(Deliver service)\n"+
+                        "Paper Craft Package 1 x"+defaltnum1+"\n"+
+                        "Paper Craft Package 2 x"+defaltnum2+"\n"+
+                        "Paper Craft Package 3 x"+defaltnum3+"\n"+
+                        "\n Total $"+ totalnum)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+
+                                finish();
+                            }
+                        }).setNegativeButton("No", null);
+
+                AlertDialog aleartDialog=builder.create();
+                aleartDialog.show();
+            }
+        });
         intstartCounter();
 
 
@@ -82,32 +132,50 @@ public class ShopPage extends AppCompatActivity {
         defaltnum1=0;
         defaltnum2=0;
         defaltnum3=0;
+        totalnum=0;
+        total_tv.setText("$"+totalnum);
         god1_edt.setText(defaltnum1+"");
         god2_edt.setText(defaltnum2+"");
         god3_edt.setText(defaltnum3+"");
+
     }
     private void AddCounter1(){
         defaltnum1++;
         god1_edt.setText(defaltnum1+"");
+        UpdateTotal();
+
     }
     private void MinusCounter1(){
         defaltnum1--;
         god1_edt.setText(defaltnum1+"");
+        UpdateTotal();
+
     }
     private void AddCounter2(){
         defaltnum2++;
         god2_edt.setText(defaltnum2+"");
+        UpdateTotal();
+
     }
     private void MinusCounter2(){
         defaltnum2--;
         god2_edt.setText(defaltnum2+"");
+        UpdateTotal();
+
     }
     private void AddCounter3(){
         defaltnum3++;
         god3_edt.setText(defaltnum3+"");
+        UpdateTotal();
+
     }
     private void MinusCounter3(){
         defaltnum3--;
         god3_edt.setText(defaltnum3+"");
+        UpdateTotal();
+    }
+    private void UpdateTotal(){
+        totalnum = defaltnum1*125 + defaltnum2*398 + defaltnum3*888;
+        total_tv.setText("$"+totalnum);
     }
 }
