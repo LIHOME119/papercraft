d90338e2619042eb0eb5e097efc97bee20d4aa31
lihome <takfu27@gmail.com>
2022-01-14 17:19:14 +0800

    trying do unit test

--- app/src/androidTest/java/com/example/papercraft/shopmathTest.java
+++ app/src/androidTest/java/com/example/papercraft/shopmathTest.java
@@ -1,7 +1,33 @@
 package com.example.papercraft;
 
-public class shopmathTest {
-    ShopPage.defaltnum1 = 2;
-    
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class shopmathTest extends InstrumentationRegistry {
+
+    @Before
+    public void test_add123equalstotal() throws Exception{
+        ShopPage.defaltnum1 = 2;
+        ShopPage.defaltnum2 = 2;
+        ShopPage.defaltnum3 = 2;
+        ShopPage.totalnum=2822;
+
+        int answer = ShopPage.defaltnum1 * 128 + ShopPage.defaltnum2 * 300 + ShopPage.defaltnum3 *888;
+
+
+    }
+
+    @Test
+    public void texttotal()throws Exception{
+        Assert(ShopPage.totalnum, answer);
+    }
+
+
+
+
+
 
 }
--- app/src/test/java/com/example/papercraft/Shopmath.java
+++ app/src/test/java/com/example/papercraft/Shopmath.java
@@ -1,6 +1,6 @@
 package com.example.papercraft;
 
-public class shopmath {
+public class Shopmath {
     public int add(int num1, int num2, int num3) {
         return num1 * 125 + num2 * 398 + num3 * 888;
 
